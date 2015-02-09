3292274ad40438f98ee3f452ae35fc03ef2aa5a7
Boris Bügling <boris@icculus.org>
2015-02-09 22:30:28 +0100

    Initial commit

---
+++ UIKit.framework/Versions/A/Headers/UIKit.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+#import <UIKit/UXKit.h>
+
+@compatibility_alias UIButton UXButton;
+@compatibility_alias UIColor NSColor;
+@compatibility_alias UITableViewController UXTableViewController;
---
+++ UXCatalog/Alias.swift
@@ -0,0 +1,17 @@
+//
+//  Alias.swift
+//  UICatalog
+//
+//  Created by Boris Bügling on 09/02/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+import UIKit
+
+//typealias UIActivityIndicatorView =
+//typealias UIAlertController =
+typealias UIButton = _UXButton;
+typealias UIColor = NSColor;
+//typealias UIDatePicker =
+typealias UITableViewController = UXTableViewController;
+typealias UIViewController = UXViewController;
